f0112f2216aa5483b80d374aa899116e2c737eec
Infectedsword64 <karpov.semenn@yandex.ru>
2025-01-24 14:54:13 +0300

    Выполнено задание на Catch2

---
+++ Testing/Testing/Testing.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cassert>
+#include <utility>
+#include "extended_array.h"
+
+void test_interval_mean() {
+    ExtArray<int> v1{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    assert(v1.mean(3, 6) == 22);
+    std::cout << "Mean test (correct interval) OK" << std::endl;
+}
+
+void test_median() {
+    ExtArray<double> v1{ 4.5, 0.7, 10.8, 2.6, -3.5 };
+    assert(v1.median() == 2.6);
+    std::cout << "Median test OK" << std::endl;
+}
+
+void test_mode() {
+    ExtArray<int> v1{ 4, 2, 7, 2, 5, -5, 0, 5, 5 };
+    assert(v1.mode() == std::make_pair(5, 3));
+    std::cout << "Mode test OK" << std::endl;
+
+
+}
+
+int main()
+{
+    test_median();
+    test_mode();
+
+    return 0;
+}
---
+++ Testing/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Testing/extended_array.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(ExtendedArrayTest) {
+public:
+		TEST_METHOD(Mean) {
+			ExtArray<int> v1{ 4, 2, 7, 3, -5, 0, -3, 1 };
+			Assert::AreEqual(v1.mean(), 1.125);
+
+			ExtArray<int> v2{ };
+			Assert::AreEqual(v2.mean(), 0.0);
+
+			ExtArray<int> v3{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+			Assert::AreEqual(v3.mean(3, 5), 0.0);
+			Assert::ExpectException<std::invalid_argument>(v3.mean(8, 3));
+			Assert::ExpectException<std::invalid_argument>(v3.mean(4, 4));
+		}
+		TEST_METHOD(Median) {
+			ExtArray<double> v1{ 4.5, 0.7, 10.8, -3.5 };
+			Assert::AreEqual(2.6, v1.median());
+
+			ExtArray<int> v2{ };
+			Assert::AreEqual(0.0, v2.median());
+		}
+		TEST_METHOD(Mode) {
+			ExtArray<int> v1{ 4, 2, 7, 2, 5, -5, 0, 5, 5 };
+			Assert::AreEqual(5, v1.mode().first);
+
+			ExtArray<int> v2{ };
+			Assert::AreEqual(0, v2.mode().first);
+		}
+	};
+}
