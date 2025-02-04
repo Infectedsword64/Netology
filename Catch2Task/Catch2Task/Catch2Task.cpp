2d70bc6960fda3f8a58ae6a854e7bfce67bead81
Infectedsword64 <karpov.semenn@yandex.ru>
2025-02-04 09:32:04 +0300

    Исправлено задание по тестированию

--- Catch2Task/Catch2Task/Catch2Task.cpp
+++ Catch2Task/Catch2Task/Catch2Task.cpp
@@ -1,99 +1,7 @@
 ﻿#include <iostream>
 #include <catch2/catch_test_macros.hpp>
-#include<catch2/catch_session.hpp>
-
-struct ListNode
-{
-public:
-    ListNode(int value, ListNode* prev = nullptr, ListNode* next = nullptr)
-        : value(value), prev(prev), next(next)
-    {
-        if (prev != nullptr) prev->next = this;
-        if (next != nullptr) next->prev = this;
-    }
-
-public:
-    int value;
-    ListNode* prev;
-    ListNode* next;
-};
-
-
-class List
-{
-public:
-    List()
-        : m_head(new ListNode(static_cast<int>(0))), m_size(0),
-        m_tail(new ListNode(0, m_head))
-    {
-    }
-
-    virtual ~List()
-    {
-        Clear();
-        delete m_head;
-        delete m_tail;
-    }
-
-    bool Empty() { return m_size == 0; }
-
-    unsigned long Size() { return m_size; }
-
-    void PushFront(int value)
-    {
-        new ListNode(value, m_head, m_head->next);
-        ++m_size;
-    }
-
-    void PushBack(int value)
-    {
-        new ListNode(value, m_tail->prev, m_tail);
-        ++m_size;
-    }
-
-    int PopFront()
-    {
-        if (Empty()) throw std::runtime_error("list is empty");
-        auto node = extractPrev(m_head->next->next);
-        int ret = node->value;
-        delete node;
-        return ret;
-    }
-
-    int PopBack()
-    {
-        if (Empty()) throw std::runtime_error("list is empty");
-        auto node = extractPrev(m_tail);
-        int ret = node->value;
-        delete node;
-        return ret;
-    }
-
-    void Clear()
-    {
-        auto current = m_head->next;
-        while (current != m_tail)
-        {
-            current = current->next;
-            delete extractPrev(current);
-        }
-    }
-
-private:
-    ListNode* extractPrev(ListNode* node)
-    {
-        auto target = node->prev;
-        target->prev->next = target->next;
-        target->next->prev = target->prev;
-        --m_size;
-        return target;
-    }
-
-private:
-    ListNode* m_head;
-    ListNode* m_tail;
-    unsigned long m_size;
-};
+#include <catch2/catch_session.hpp>
+#include "My_list.h"
 
 
 int main(int argc, char* argv[]) {
@@ -103,6 +11,8 @@ int main(int argc, char* argv[]) {
 TEST_CASE("test list", "[list]") {
     SECTION("Emptiness") {
         List list;
+        REQUIRE_THROWS(list.PopBack());
+        REQUIRE_THROWS(list.PopFront());
         CHECK(list.Empty() == true);
         list.PushFront(100);
         CHECK(list.Empty() == false);
@@ -143,7 +53,9 @@ TEST_CASE("test list", "[list]") {
         CHECK(list.Size() == 4);
     }
     SECTION("PopBack") {
+
         List list;
+        REQUIRE_THROWS(list.PopBack());
         list.PushFront(10);
         list.PushFront(30);
         list.PushFront(30);
@@ -154,6 +66,7 @@ TEST_CASE("test list", "[list]") {
     }
     SECTION("PopFront") {
         List list;
+        REQUIRE_THROWS(list.PopFront());
         list.PushFront(10);
         list.PushFront(30);
         list.PushFront(30);
@@ -162,5 +75,6 @@ TEST_CASE("test list", "[list]") {
         list.PopFront();
         CHECK(list.Size() == 2);
     }
+
     system("pause");
 }
