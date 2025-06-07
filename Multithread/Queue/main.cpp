cb6b3f58b2eefebbc469c423889edc4bca372f6b
Infectedsword64 <karpov.semenn@yandex.ru>
2025-06-07 16:14:18 +0300

    Выполнено задание по многопоточности

---
+++ Multithread/Queue/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <functional>
+
+int maxClients = 10;
+int clientCounter = 0;
+
+void clientThread() {
+    while (clientCounter < maxClients) 
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        clientCounter++;
+        std::cout << "Client joined, total clients: " << clientCounter << std::endl;
+    }
+}
+
+void operatorThread() {
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        if (clientCounter > 0) 
+        {
+            clientCounter--;
+            std::cout << "The operator served the client. Number of clients: " << clientCounter << std::endl;
+        }
+        else 
+        {
+            std::cout << "The operator has finished his work. Number of clients: " << clientCounter << std::endl;
+            break;
+        }
+    }
+}
+
+int main() {
+    std::thread client(clientThread);
+    std::thread operatorT(operatorThread);
+
+    client.join();
+    operatorT.join();
+
+    return 0;
+}
